7556548217b388640b6f257dc598b7fcbf79fedd
petscream <petscreamnyc@gmail.com>
2026-01-04 21:26:59 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>Extra. On purpose.</h1>
+      <p>Not essential. Intentional.</p>
+      <p>Petscream website is live 🐾🍦</p>
+    </main>
+  );
+}
