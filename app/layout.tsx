859ce60f2e20e0f2dafdd7bc61fbb743d9053186
petscream <petscreamnyc@gmail.com>
2026-01-05 02:13:11 +0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Petscream",
+  description: "Not essential. Intentional.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
