1ff6d0a7881441a862f34a519979deeb1d73177a
petscream <petscreamnyc@gmail.com>
2026-01-05 07:03:37 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { ReactNode } from "react";
 
 export const metadata = {
   title: "Petscream",
@@ -8,7 +9,7 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   return (
     <html lang="en">
