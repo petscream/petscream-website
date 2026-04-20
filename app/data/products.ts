5f0c6c9dc0c110f48f6c6d25a53a67cd10dc22f4
petscream <petscreamnyc@gmail.com>
2026-04-20 09:47:17 +0300

    Update products.ts

--- app/data/products.ts
+++ app/data/products.ts
@@ -1,39 +1,32 @@
-export type Product = {
-  id: string;
-  name: string;
-  subtitle: string;
-  image: string;
-};
-
-export const products: Product[] = [
+export const products = [
   {
     id: "heart-pop",
     name: "Heart Pop",
     subtitle: "Blueberry & Banana",
-    image: "/products/heart-pop.png",
+    image: "/images/heart-pop.png",
   },
   {
     id: "paw-pop",
     name: "Paw Pop",
     subtitle: "Blueberry & Banana",
-    image: "/products/paw-pop.png",
+    image: "/images/paw-pop.png",
   },
   {
     id: "mini-pop",
     name: "Mini Pop",
     subtitle: "Blueberry & Banana",
-    image: "/products/mini-pop.png",
+    image: "/images/mini-pop.png",
   },
   {
     id: "mini-paw-treats",
     name: "Mini Paw Treats",
     subtitle: "Blueberry & Banana",
-    image: "/products/mini-paw-treats.png",
+    image: "/images/mini-paw-treats.png",
   },
   {
     id: "mini-bone-treats",
     name: "Mini Bone Treats",
     subtitle: "Blueberry & Banana",
-    image: "/products/mini-bone-treats.png",
+    image: "/images/mini-bone-treats.png",
   },
 ];
