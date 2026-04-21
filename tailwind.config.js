f8b8d6494dbc8f74bb4db4b40eed4f3a841a03fe
petscream <petscreamnyc@gmail.com>
2026-04-21 05:49:00 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  safelist: [
+    "grid",
+    "gap-8",
+    "sm:grid-cols-2",
+    "lg:grid-cols-3",
+    "xl:grid-cols-3",
+    "flex",
+    "flex-col",
+    "md:flex-row",
+    "min-h-screen",
+    "max-w-7xl",
+    "mx-auto",
+    "px-6",
+    "py-16",
+    "text-center",
+    "font-extrabold",
+    "rounded-full",
+    "bg-\\[\\#2FB7B5\\]",
   ],
   theme: {
     extend: {},
