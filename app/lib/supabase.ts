93e9b264c13714e2ca5b58f8af38470b4b4f48e8
petscream <petscreamnyc@gmail.com>
2026-05-10 21:46:48 +0300

    Fix Supabase client

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  'https://ghvmubrcmhlujquqpzuf.supabase.co',
+  'sb_publishable_pLAYB_hP7mDigQgSPxHd_w_whsN1Wmh'
+)
