9edf229a6ec7e2b5185fbb0083863f1961a4b6b9
petscream <petscreamnyc@gmail.com>
2026-05-10 21:56:13 +0300

    fix

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://ghvmubrcmhlujquqpzuf.supabase.co',
-  'sb_publishable_pLAYB_hP7mDigQgSPxHd_w_whsN1Wmh'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdodm11YnJjbWhsdWpxdXFwenVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzOTA1NzQsImV4cCI6MjA5Mzk2NjU3NH0.6jIxPwq1g5jK_fpTYcFVNQHJBohr6DWBuZtjrbo-bic'
 )
